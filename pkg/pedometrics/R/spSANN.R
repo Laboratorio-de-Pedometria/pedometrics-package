5dcfb46250808e733c5a9104ec1d3640bb1eed09
alessandro <alessandro@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2014-10-13 21:38:48 +0000

    Small modifications... functions not operational!

--- pkg/pedometrics/R/spSANN.R
+++ pkg/pedometrics/R/spSANN.R
@@ -19,7 +19,6 @@
 #                   E. Pebesma (edzer.pebesma@uni-muenster.de)
 #                   J. Skoien (jon.skoien@gmail.com)
 #
-# get objective function value
 energyState <- 
   function (fun, obj, ...) {
     if (missing(fun) || missing(obj)) {
@@ -27,71 +26,22 @@ energyState <-
     }
     return (do.call(fun, list(obj, ...)))
   }
-# spatial simulated annealing
 spSANN <-
-  function (obj, fun, iterations = 10000, spJitter.ctrl = spJitter.control(),
-            size = 1, 
-            size.factor = iterations / 10,
-            acceptance = list(fun = "Default", initial = 0.99, 
-                              cooling.factor = iterations / 10),
-            temperature = NULL, 
+  function (obj, fun, iterations = 10000, 
+            spJitter.ctrl = spJitter.control(),
+            acceptance = list(initial = 0.99, cooling = iterations / 10),
             stopping = list(max.count = 200),
             progress = TRUE, verbose = TRUE,
             plotit = list(prob = TRUE, starting = TRUE,
                           x.max = FALSE, y.max = FALSE), ...) {
     if (missing(obj)) {
       stop ("'obj' is a mandatory argument")
-    } else {
-      if (!inherits(obj, "SpatialPoints") || is.na(proj4string(obj)) || 
-            !is.projected(obj)) {
-        stop ("'obj' should be of class SpatialPoints with a projected CRS")
-      }  
     }
-    if (missing(fun)) {
-      stop ("'fun' is a mandatory argument")
-    } else {
-      if (!is.function(fun)) {
-        stop ("'fun' should be a function")
-      }  
+    if (missing(fun) || !is.function(fun)) {
+      stop ("'fun' is a mandatory function")
     }
     if (!is.numeric(iterations) || length(iterations) > 1) {
-      stop ("'iterations' should be a numeric value")
-    }
-    if (is.null(spJitter.ctrl$x.coord$max)) {
-      if (!is.null(spJitter.ctrl$candidates)) {
-        dx <- bbox(spJitter.ctrl$candidates)["x", "max"] - 
-          bbox(spJitter.ctrl$candidates)["x", "min"]
-        spJitter.ctrl$x.coord$max <- dx * spJitter.ctrl$x.coord$factor
-      } else {
-        if (!is.null(spJitter.ctrl$where)) {
-          dx <- bbox(spJitter.ctrl$where)["x", "max"] - 
-            bbox(spJitter.ctrl$where)["x", "min"]
-          spJitter.ctrl$x.coord$max <- dx * spJitter.ctrl$x.coord$factor
-        } else {
-          dx <- bbox(obj)["x", "max"] - bbox(obj)["x", "min"]
-          spJitter.ctrl$x.coord$max <- dx * spJitter.ctrl$x.coord$factor
-        }
-      }
-    } else {
-      spJitter.ctrl$x.coord$max <- spJitter.ctrl$x.coord$max
-    }
-    if (is.null(spJitter.ctrl$y.coord$max)) {
-      if (!is.null(spJitter.ctrl$candidates)) {
-        dx <- bbox(spJitter.ctrl$candidates)["y", "max"] - 
-          bbox(spJitter.ctrl$candidates)["y", "min"]
-        spJitter.ctrl$y.coord$max <- dx * spJitter.ctrl$y.coord$factor
-      } else {
-        if (!is.null(spJitter.ctrl$where)) {
-          dx <- bbox(spJitter.ctrl$where)["y", "max"] - 
-            bbox(spJitter.ctrl$where)["y", "min"]
-          spJitter.ctrl$y.coord$max <- dx * spJitter.ctrl$y.coord$factor
-        } else {
-          dx <- bbox(obj)["y", "max"] - bbox(obj)["y", "min"]
-          spJitter.ctrl$y.coord$max <- dx * spJitter.ctrl$y.coord$factor
-        }
-      }
-    } else {
-      spJitter.ctrl$y.coord$max <- spJitter.ctrl$y.coord$max
+      stop ("'iterations' should be a numeric value") 
     }
     if (!is.numeric(stopping$max.count) || length(stopping$max.count) > 1) {
       stop ("'max.count' should be a numeric value")
@@ -99,34 +49,37 @@ spSANN <-
     if (!is.numeric(size) || length(size) > 1 || size < 1) {
       stop ("'size' should be a positive numeric value")
     }
+    if (!is.list(acceptance) || length(acceptance) != 2) {
+      stop ("'acceptance' should be a list with 2 subarguments")
+    }
     if (!is.logical(progress)) {
       stop ("'progress' should be a logical value")
     }
     if (!is.logical(verbose)) {
       stop ("'verbose' should be a logical value")
     }
-    time0             <- proc.time()
     n_pts             <- length(obj)
     sys_config0       <- obj
     old_sys_config    <- sys_config0
     energy_state0     <- energyState(fun = fun, obj = old_sys_config, ...)
     old_energy_state  <- energy_state0
     count             <- 0
-    nr_designs        <- 1
     best_energy_state <- Inf
     energy_states     <- vector()
     accept_probs      <- vector()
     x_max             <- vector()
-    x_max0            <- spJitter.ctrl$x.coord$max
     y_max             <- vector()
+    x_max0            <- spJitter.ctrl$x.coord$max
     y_max0            <- spJitter.ctrl$y.coord$max
+    size              <- spJitter.ctrl$size
+    size.factor       <- spJitter.ctrl$size.factor
     if (progress) {
       pb <- txtProgressBar(min = 1, max = iterations, style = 3)
     }
+    time0             <- proc.time()
     for (k in 1:iterations) {
       if (size > 1) {
         new_size <- round(c(size - 1) * exp(-k / size.factor) + 1)
-        #new_size <- round(size - k / iterations * (size - 1))
       } else {
         new_size <- size
       }
@@ -147,37 +100,16 @@ spSANN <-
       y_max[k] <- spJitter.ctrl$y.coord$max
       new_energy_state <- energyState(fun = fun, obj = new_sys_config, ...)
       random_prob <- runif(1)
-      # TEMPERATURE
-      # if (!is.null(temperature)) {
-      #   if (temperature$fun == "exp") {
-      #     temp <- temperature$initial * alpha ^ k
-      #   }
-      #   if (temperature$fun == "fast") {
-      #     temp <- temperature$initial / k
-      #   }
-      #   if (temperature.fun == "boltz") {
-      #     temp <- temperature$initial / ln(k)
-      #   } 
-      # }
-      if (acceptance$fun == "Default") {
-        actual_prob <- acceptance$initial * exp(-k / acceptance$cooling.factor)
-      } else {
-        if (acceptance$fun == "Metropolis") {
-          delta <- new_energy_state - old_energy_state
-          actual_prob <- exp(-delta / temp)
-        }
-      }
+      actual_prob <- acceptance$initial * exp(-k / acceptance$cooling)
       accept_probs[k] <- actual_prob
       if (new_energy_state <= old_energy_state) {
         old_sys_config <- new_sys_config
         old_energy_state <- new_energy_state
-        nr_designs <- nr_designs + 1
         count <- 0
         } else {
           if (new_energy_state > old_energy_state & random_prob <= actual_prob) {
             old_sys_config <- new_sys_config
             old_energy_state <- new_energy_state
-            nr_designs <- nr_designs + 1
             count <- count + 1
             if (verbose) {
               if (count == 1) {
@@ -191,7 +123,6 @@ spSANN <-
             } else {
               new_energy_state <- old_energy_state
               new_sys_config <- old_sys_config
-              nr_designs <- nr_designs
               count <- count + 1
               if (verbose) {
                 if (count == 1) {
@@ -266,7 +197,6 @@ spSANN <-
           new_sys_config <- best_sys_config
           old_energy_state <- best_old_energy_state
           new_energy_state <- best_energy_state
-          nr_designs <- nr_designs + 1
           count <- 0
           cat("\n", "reached 'max.count' with suboptimal system configuration\n")
           cat("\n", "restarting with previously best system configuration\n")
