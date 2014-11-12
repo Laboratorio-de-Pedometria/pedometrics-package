df910f823153642ac2ff993f108daf9bc5a542dd
alessandro <alessandro@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2014-11-12 14:54:33 +0000

    Several changes were made. spSANN and spJitter now work only with a finite set of locations. The data has now to be entered in tje form of a matrix or data.frame instead of sp data.

--- R/objMSSD.R
+++ R/objMSSD.R
@@ -18,64 +18,49 @@
 #  Contributions  : G. Heuvelink (gerard.heuvelink@wur.nl)
 #                   R. Hijmans (r.hijmans@gmail.com)
 #
-.distMat <- 
-  function (obj, exponent = 1, diagonal = 0) {
-    dist_mat <- dist(obj)
+distMat <- 
+  function (candidates, exponent = 1, diagonal = 0) {
+    dist_mat <- dist(candidates)
     dist_mat <- as.matrix(dist_mat)
     dist_mat <- dist_mat ^ exponent
     diag(dist_mat) <- diagonal
     return (dist_mat)
   }
-.distanceToNearestPoint <- 
-  function (obj, points) {
-    if (dim(obj)[1] != dim(obj)[2] || !is.matrix(obj)) {
-      stop ("'obj' should be a n x n matrix")
+distToNearestPoint <-
+  function (dist.mat, which.pts) {
+    if (dim(dist.mat)[1] != dim(dist.mat)[2] || !is.matrix(dist.mat)) {
+      stop ("'dist.mat' should be a n x n matrix")
     }
-    if (!is.vector(points) || length(points) >= dim(obj)[2]) {
-      stop (paste("'points' should be a vector of length smaller than ", 
-                  dim(obj)[2], sep = ""))
+    if (length(which.pts) >= dim(dist.mat)[2]) {
+      stop (paste("'which.pts' should be a vector of length smaller than ", 
+                  dim(dist.mat)[2], sep = ""))
     }
-    sub_dist_mat <- obj[, points]
-    min_dist <- apply(sub_dist_mat, MARGIN = 1, FUN = min)
+    sub_dist_mat <- dist.mat[, which.pts]
+    min_dist <- as.numeric(apply(sub_dist_mat, MARGIN = 1, FUN = min))
     return (min_dist)
   }
-.nearestPoint <- 
-  function (obj, points) {
-    if (dim(obj)[1] != dim(obj)[2] || !is.matrix(obj)) {
-      stop ("'obj' should be a n x n matrix")
+nearestPoint <-
+  function (dist.mat, which.pts) {
+    if (dim(dist.mat)[1] != dim(dist.mat)[2] || !is.matrix(dist.mat)) {
+      stop ("'dist.mat' should be a n x n matrix")
     }
-    if (!is.vector(points) || length(points) >= dim(obj)[2]) {
-      stop (paste("'points' should be a vector of length smaller than ", 
-                  dim(obj)[2], sep = ""))
+    if (!is.vector(which.pts) || length(which.pts) >= dim(dist.mat)[2]) {
+      stop (paste("'which.pts' should be a vector of length smaller than ", 
+                  dim(dist.mat)[2], sep = ""))
     }
-    sub_dist_mat <- obj[, points]
+    sub_dist_mat <- dist.mat[, which.pts]
     min_dist_id <- apply(sub_dist_mat, MARGIN = 1, FUN = which.min)
     return (min_dist_id)
   }
 objMSSD <-
-  function (obj, pred.grid, mask, dist.mat, finite) {
-    if (missing(finite)) {
-      stop("you must provide a 'finite' argument (TRUE/FALSE)")
+  function (points, pred.grid, dist.mat) {
+    which_pts <- duplicated(rbind(points, pred.grid))
+    which_pts <- which(which_pts[-c(1:dim(points)[1])])
+    if (dim(dist.mat)[1] != dim(dist.mat)[2] || !is.matrix(dist.mat)) {
+      stop ("'dist.mat' should be a n x n matrix")
     }
-    stopifnot(is.logical(finite))
-    if (!finite) {
-      res <- distanceFromPoints(object = pred.grid, xy = obj) ^ 2
-      if (!missing(mask)) {
-        res <- raster::mask(x = res, mask = mask)  
-      }
-      res <- mean(res@data@values, na.rm = TRUE)
-      return (res)
-    } else {
-      if (dim(dist.mat)[1] != dim(dist.mat)[2] || !is.matrix(dist.mat)) {
-        stop ("'dist.mat' should be a n x n matrix")
-      }
-      if (!is.vector(obj) || length(obj) >= dim(dist.mat)[2]) {
-        stop (paste("'obj' should be a vector of length smaller than ", 
-                    dim(dist.mat)[2], sep = ""))
-      }
-      res <- .distanceToNearestPoint(obj = dist.mat, points = obj)
-      res <- mean(res, na.rm = TRUE)
-      return (res)
-    }
+    res <- distToNearestPoint(dist.mat = dist.mat, which.pts = which_pts)
+    res <- mean(res, na.rm = TRUE)
+    return (res)
   }
 # End!
