a4e5d831345bfdcf541cf4c9861c26545f7285d9
alessandro <alessandro@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2014-11-26 23:03:52 +0000

    preparing to produce documentation using roxygen2

--- pkg/pedometrics/R/adjR2.R
+++ pkg/pedometrics/R/adjR2.R
@@ -1,3 +1,5 @@
+#  file pedometrics/R/adjR2.R
+#
 #  This program is free software; you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
 #  the Free Software Foundation; either version 2 or 3 of the License
@@ -11,15 +13,42 @@
 #  A copy of the GNU General Public License is available at
 #  http://www.r-project.org/Licenses/
 #
-#  Purpose        : Get model statistics
-#  Maintainer     : A. Samuel-Rosa (alessandrosamuelrosa@gmail.com)
-#  Contributions  : 
-#  Version        : beta
-#  Depends on     : 
-#  Dependency of  :
+# DOCUMENTATION ################################################################
+#' 
+#' @title Adjusted coefficient of determination
+#' 
+#' @description
+#' Calculates the adjusted coefficient of determination of a multiple linear 
+#' regression model.
+#' 
+#' @param r2 Numeric vector with the coeficient of deternmination to be 
+#' adjusted.
+#' @param n Numeric vector providing the number of observations used to fit the
+#' multiple linear regression model.
+#' @param p Numeric vector providing the number of parameters included in the
+#' multiple linear regression model.
+#' 
+#' @details Details will be added later.
+#' 
+#' @return A numeric vector with the adjusted coefficient of determination.
+#' 
+#' @author Alessandro Samuel-Rosa \email{alessandrosamuelrosa@gmail.com}
+#' 
+#' @references Coefficient of determination. Wikipedia, The Free Encyclopedia.
+#' Available at \url{http://en.wikipedia.org/wiki/Coefficient_of_determination}.
+#' [Online; accessed 31-July-2014].
+#' 
+#' @examples
+#' adjR2(r2 = 0.95, n = 100, p = 80)
+#' 
+#' @keywords misc
+#' 
+# FUNCTION #####################################################################
+#
 adjR2 <- 
   function (r2, n, p) {
     r2 <- 1 - (1 - r2) * ((n - 1) / (n - p - 1))
     return(r2)
   }
+#
 # End!
