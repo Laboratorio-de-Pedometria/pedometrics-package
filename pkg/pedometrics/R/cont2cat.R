4de350048ee56fd5014b275cad1706b41598b341
alessandro <alessandro@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2015-01-09 00:03:59 +0000

    documented cont2cat

--- pkg/pedometrics/R/cont2cat.R
+++ pkg/pedometrics/R/cont2cat.R
@@ -1,22 +1,42 @@
 #' Continuous to categorical
 #' 
-#' Convert a continuous variable into a categorical one.
+#' Convert continuous data into categorical data.
 #' 
-#' @param x Data frame or matrix with the continuous variables to be converted
-#' into categorical variables (factors).
+#' @param x Data frame or matrix with the continuous data to be converted
+#' into categorical data (factors).
 #' 
 #' @param breaks List with the lower and upper limits to be used to break the
-#' continuous variable. Using a list allows breaking the variables into a
+#' continuous data. Using a list allows breaking the data into a
 #' different number of classes.
 #' 
-#' @return
-#' data.frame
-#' 
+#' @return A data frame or matrix with the categorical data.
+#' @author Alessandro Samuel-Rosa \email{alessandrosamuelrosa@@gmail.com}
+#' @seealso \code{\link[Hmisc]{cut2}}
+#' @import Hmisc
+#' @examples
+#' require(Hmisc)
+#' # Matrix
+#' x <- c(1:80)
+#' x <- cbind(x, x)
+#' breaks <- list(c(1, 10, 20, 40, 80), c(1, 50, 100))
+#' y <- cont2cat(x, breaks)
+#' y
+#' # Data frame
+#' x <- c(1:80)
+#' x <- data.frame(cbind(x, x))
+#' breaks <- list(c(1, 10, 20, 40, 80), c(1, 50, 100))
+#' y <- cont2cat(x, breaks)
+#' y
 # MAIN FUNCTION ################################################################
 cont2cat <-
   function (x, breaks) {
-    for (i in 1:ncol(x)) {
+    cl <- class(x)
+    n_col <- ncol(x)
+    for (i in 1:n_col) {
       x[, i] <- cut2(x[, i], breaks[[i]])
     }
+    x <- lapply(x, as.integer)
+    if (cl == "data.frame") x <- as.data.frame(x)
+    if (cl == "matrix") x <- matrix(x, ncol = n_col)
     return (x)
   }
