377d89ab9fc941974dc7792b4c2dbed4663ad898
alessandro <alessandro@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2015-01-11 16:33:49 +0000

    Continuous to categorical

--- R/cont2cat.R
+++ R/cont2cat.R
@@ -1,43 +1,54 @@
 #' Continuous to categorical
 #' 
-#' Convert continuous data into categorical data.
+#' Convert continuous data (numeric) into categorical data (factor or integer).
 #' 
-#' @param x Data frame or matrix with the continuous data to be converted
-#' into categorical data (factors).
+#' @param x Vector, data frame or matrix with the continuous data to be 
+#' converted into categorical data.
 #' 
-#' @param breaks List with the lower and upper limits to be used to break the
-#' continuous data. Using a list allows breaking the data into a
-#' different number of classes.
+#' @param breaks Vector or list with the lower and upper limits to be used to 
+#' break the continuous data into classes.
 #' 
-#' @return A data frame or matrix with the categorical data.
+#' @details
+#' Breaks must be a vector if \code{x} is a vector, but a list if \code{x} is a
+#' data frame or matrix. Using a list allows breaking the data into a different
+#' number of classes.
+#' 
+#' @return
+#' A vector, data frame, or matrix, depending on the class of \code{x}.
+#' 
 #' @author Alessandro Samuel-Rosa \email{alessandrosamuelrosa@@gmail.com}
 #' @seealso \code{\link[Hmisc]{cut2}}
+#' @export
 #' @import Hmisc
 #' @examples
 #' require(Hmisc)
 #' # Matrix
-#' x <- c(1:80)
+#' x <- c(1:10)
 #' x <- cbind(x, x)
-#' breaks <- list(c(1, 10, 20, 40, 80), c(1, 50, 100))
+#' breaks <- list(c(1, 2, 4, 8, 10), c(1, 5, 10))
 #' y <- cont2cat(x, breaks)
 #' y
 #' # Data frame
-#' x <- c(1:80)
+#' x <- c(1:10)
 #' x <- data.frame(cbind(x, x))
-#' breaks <- list(c(1, 10, 20, 40, 80), c(1, 50, 100))
+#' breaks <- list(c(1, 2, 4, 8, 10), c(1, 5, 10))
+#' y <- cont2cat(x, breaks)
+#' y
+#' # Vector
+#' x <- c(1:10)
+#' breaks <- c(1, 2, 4, 8, 10)
 #' y <- cont2cat(x, breaks)
 #' y
 # MAIN FUNCTION ################################################################
 cont2cat <-
   function (x, breaks) {
-    cl <- class(x)
-    n_col <- ncol(x)
-    for (i in 1:n_col) {
-      x[, i] <- cut2(x[, i], breaks[[i]])
+    if (is.vector(x) && is.vector(breaks)) {
+      x <- cut2(x, breaks)
+    } else {
+      n_col <- ncol(x)
+      for (i in 1:n_col) {
+        x[, i] <- cut2(x[, i], breaks[[i]])
+      }
     }
-    x <- lapply(x, as.integer)
-    x <- lapply(x, as.factor)
-    if (cl == "data.frame") x <- as.data.frame(x)
-    if (cl == "matrix") x <- matrix(x, ncol = n_col)
     return (x)
   }
