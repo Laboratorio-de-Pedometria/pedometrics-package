bcc327ebd7f2d3b7dc33b85a4a303bd510cadac7
samuel-rosa <alessandrosamuelrosa@gmail.com>
2015-03-03 13:42:15 -0300

    Version 0.6-1

--- R/numint.R
+++ R/numint.R
@@ -1,17 +1,19 @@
-#' Numeric integer vectors
+#' Tests for data types
 #' 
-#' Test for objects of type \code{"numeric"} and \code{"integer"}.
+#' Evaluate the data type contained in an object.
 #' 
 #' @param x Object to be tested.
 #' 
 #' @return
-#' \code{TRUE} or \code{FALSE} depending on whether \code{x} is a numeric
-#' integer or not.
+#' \code{TRUE} or \code{FALSE} depending on whether \code{x} contains a given
+#' data type.
 #' 
-#' @seealso \code{\link[base]{is.numeric}}, \code{\link[base]{is.integer}}
+#' @seealso \code{\link[base]{is.numeric}}, \code{\link[base]{is.integer}},
+#' \code{\link[base]{is.factor}}.
 #' 
 #' @author Alessandro Samuel-Rosa \email{alessandrosamuelrosa@@gmail.com}
-#' 
+#' @aliases is.numint is.all.numint is.all.integer is.all.factor is.any.factor
+#' is.all.numeric is.one.type
 #' @export
 #' @examples
 #' # Vector of integers
@@ -27,7 +29,7 @@
 #' is.numint(1) # TRUE
 #' # Single numeric value
 #' is.numint(1.1) # FALSE
-# FUNCTION #####################################################################
+# FUNCTION - IS NUMERIC INTEGER? ###############################################
 is.numint <-
   function (x) {
     if (is.integer(x) || is.factor(x) || is.character(x)) return (FALSE)
@@ -37,5 +39,60 @@ is.numint <-
     } else {
       res <- ifelse(round(x, digits = 0) == x, TRUE, FALSE)
     }
+    return (res)
+  }
+# FUNCTION - ARE ALL NUMERIC INTEGERS? #########################################
+#' @rdname is.numint
+#' @export
+is.all.numint <-
+  function (x) {
+    res <- sapply(x, is.numint)
+    res <- all(res == TRUE)
+    return (res)
+  }
+# FUNCTION - ARE ALL INTEGERS? #################################################
+#' @rdname is.numint
+#' @export
+is.all.integer <-
+  function (x) {
+    res <- sapply(x, is.integer)
+    res <- all(res == TRUE)
+    return (res)
+  }
+# FUNCTION - ARE ALL FACTORS? ##################################################
+#' @rdname is.numint
+#' @export
+is.all.factor <-
+  function (x) {
+    res <- sapply(x, is.factor)
+    res <- all(res == TRUE)
+    return (res)
+  }
+# FUNCTION - IS ANY FACTOR? ####################################################
+#' @rdname is.numint
+#' @export
+is.any.factor <-
+  function (x) {
+    res <- sapply(x, is.factor)
+    res <- any(res == TRUE)
+    return (res)
+  }
+# FUNCTION - ARE ALL NUMERIC ###################################################
+#' @rdname is.numint
+#' @export
+is.all.numeric <-
+  function (x) {
+    res <- sapply(x, is.numeric)
+    res <- all(res == TRUE)
+    return (res)
+  }
+# FUNCTION - IS ONE TYPE #######################################################
+#' @rdname is.numint
+#' @export
+is.one.type <-
+  function (x) {
+    res <- sapply(x, class)
+    res <- length(unique(res))
+    res <- ifelse(res == 1, TRUE, FALSE)
     return (res)
   }
