2d451d4c7bf8090d7340f97aab0609ce801ea53d
samuel-rosa <samuel-rosa@users.noreply.github.com>
2015-07-25 20:39:40 -0300

    import non-default functions

---
+++ R/AAAA.R
@@ -0,0 +1,5 @@
+#  Import functions from non-default packages
+#' @importFrom grDevices cm.colors extendrange
+#' @importFrom graphics axis legend lines mtext par title
+#' @importFrom stats dnorm extractAIC formula lm median model.frame 
+#' @importFrom stats model.matrix residuals sd update
