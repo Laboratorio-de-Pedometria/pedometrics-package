f3e052cecac91c196605197ae187726a5ee73b6d
samuel-rosa <samuel-rosa@users.noreply.github.com>
2015-11-27 15:31:27 -0200

    improved documentation and check suggested package

--- R/vgmLags.R
+++ R/vgmLags.R
@@ -2,29 +2,32 @@
 #' 
 #' Computation of lag-distance classes for varioram estimation.
 #' 
-#' @param obj Data frame or matrix; the x and y coordinates (the coordinates 
-#' must be projected).
+#' @param obj Data frame or matrix with the projected x- and y-coordinates.
 #' 
-#' @param n Integer value; the number of lag-distace classes. Defaults to 
-#' \code{n = 7}.
+#' @param n Integer value defining the number of lag-distance classes that 
+#' should be computed. Defaults to \code{n = 7}.
 #' 
-#' @param type Caracter value; the type of lag-distance classes, with options
-#' \code{equi} (equidistant) and \code{exp} (exponential). Defaults to 
-#' \code{type = "exp"}.
+#' @param type Character value defining the type of lag-distance classes that
+#' should be computed, with options \code{"equi"} (equidistant) and \code{"exp"}
+#' (exponential). Defaults to \code{type = "exp"}.
 #' 
-#' @param cutoff Numeric value; the fraction of the maximum pair-wise sepation
-#' distances that should be used to compute the lag-distance classes. 
-#' Defaults to \code{cutoff = 0.5}.
+#' @param cutoff Numeric value defining the fraction of the diagonal of the
+#' rectangle that spans the data (bounding box) that should be used to set the
+#' maximum distance up to which lag-distance classes should be computed. 
+#' Defaults to \code{cutoff = 0.5}, i.e. half the diagonal of the bounding box.
 #' 
-#' @param base Numeric value; the base of the exponential expression used to
-#' create exponentially spaced lag-distance classes. Used only when 
-#' \code{type = "exp"}. Defaults to \code{base = 2}.
+#' @param base Numeric value defining the base of the exponential expression 
+#' used to create exponentially spaced lag-distance classes. Used only when 
+#' \code{type = "exp"}. Defaults to \code{base = 2}, i.e. the width of the 
+#' rightmost lag-distance classes is equal to half the diagonal of 
+#' \code{cutoff}, and so on.
 #' 
-#' @param zero Numeric value; the minimum pair-wise separation distance that
-#' should be used to compute the lag-distance classes. Defaults to 
+#' @param zero Numeric value setting the minimum pair-wise separation distance 
+#' that should be used to compute the lag-distance classes. Defaults to 
 #' \code{zero = 0.0001}.
 #' 
-#' @return Vector of numeric values with the breaks of the lag-distance classes.
+#' @return Vector of numeric values with the lower and upper boundaries of the 
+#' lag-distance classes.
 #' 
 #' @author Alessandro Samuel-Rosa <\email{alessandrosamuelrosa@@gmail.com}>
 #' @seealso \code{\link[spsann]{optimPPL}}
@@ -37,14 +40,22 @@
 #' @examples
 #' require(sp)
 #' data(meuse.grid)
-#' id <- sample(nrow(meuse.grid), 100)
-#' obj <- meuse.grid[id, 1:2]
-#' vgmLags(obj)
+#' vgmLags(meuse.grid[, 1:2])
 # FUNCTION - MAIN ##############################################################
 vgmLags <-
   function (obj, n = 7, type = "exp", cutoff = 0.5, base = 2, zero = 0.001) {
     
+    # Check if suggested packages are installed
+    pkg <- c("sp")
+    id <- !sapply(pkg, requireNamespace, quietly = TRUE)
+    if (any(id)) {
+      pkg <- paste(pkg[which(id)], collapse = " ")
+      stop(paste("Package(s) needed for this function to work but not",
+                 "installed: ", pkg, sep = ""), call. = FALSE)
+    }
+    
     # Compute cutoff
+    
     cutoff <- max(stats::dist(obj)) * cutoff
     
     # Equidistant lag distance classes  
