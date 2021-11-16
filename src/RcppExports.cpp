f507e8eb369437d17277233c1f552d945d0924d8
Alessandro Samuel-Rosa <alessandrosamuelrosa@gmail.com>
2021-11-16 11:54:33 -0300

    it appears to have worked

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -5,6 +5,11 @@
 
 using namespace Rcpp;
 
+#ifdef RCPP_USE_GLOBAL_ROSTREAM
+Rcpp::Rostream<true>&  Rcpp::Rcout = Rcpp::Rcpp_cout_get();
+Rcpp::Rostream<false>& Rcpp::Rcerr = Rcpp::Rcpp_cerr_get();
+#endif
+
 // rowMinCpp
 NumericVector rowMinCpp(NumericMatrix x);
 RcppExport SEXP _pedometrics_rowMinCpp(SEXP xSEXP) {
